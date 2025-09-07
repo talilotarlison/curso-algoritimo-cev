e9cfef4265a53bf2543ea03c7dfde216a9e71b84
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-09-07 20:56:14 -0300

    Update memoria.c

--- material/memoria.c
+++ material/memoria.c
@@ -1,2 +1,14 @@
 
-https://www-quora-com.translate.goog/Can-two-variables-have-the-same-address-in-C?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc
+//https://www-quora-com.translate.goog/Can-two-variables-have-the-same-address-in-C?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc
+
+#include <stdio.h>
+
+int main()
+{
+    int i=3; 
+    int* pi = &i; // pi contém o endereço de i. 
+    *pi = 4; // Agora i é 4 
+    printf("Agora i é %d\n", i); 
+
+    return 0;
+}
